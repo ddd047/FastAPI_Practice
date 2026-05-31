78d7dca0a71b3844c6055554f021f5fe109d921f
ddd047 <himanshu.hry26@gmail.com>
2026-05-31 21:56:35 +0530

    feat: complete initial routing setup and clean up main.py

--- my-new-folder/main.py
+++ my-new-folder/main.py
@@ -13,3 +13,9 @@ def hello():
 @app.get("/about")
 def new():
     return {"message": "I am Himanshu. And I am Learning FastAPI"}
+
+
+# to initialize your web server run "uvicorn main:app --reload" which gives requests and ctrl+C to end
+# while you can run your local server like http://127.0.0.1:8000, this is created when i run the command.
+# if you change the command to http://http://127.0.0.1:8000/about it will show the second message.
+# if you change the command to http://http://127.0.0.1:8000/docs it will give you the documentation of your FastAPI
