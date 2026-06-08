37b03ab855893ac7f8de29afa1b02210aa3e8d04
ddd047 <himanshu.hry26@gmail.com>
2026-06-08 21:29:27 +0530

    new folder

--- project_1/main.py
+++ project_1/main.py
@@ -1,21 +1,35 @@
-from fastapi import FastAPI  # type: ignore[import]
+from fastapi import FastAPI
+import json
 
 # create a object of the FastAPI class to define endpoints
 app = FastAPI()
 
 
+def load_data():  # A function to retrieve data from the json file
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+
+
 # defining a root for endpoint
 @app.get("/")
 def hello():
-    return {"message": "HELLO WORLD !"}
+    return {"message": "Patient Management System API"}
 
 
 @app.get("/about")
 def new():
-    return {"message": "I am Himanshu. And I am Learning FastAPI"}
+    return {"message": "A fully functional API for managing patient records"}
 
 
 # to initialize your web server run "uvicorn main:app --reload" which gives requests and ctrl+C to end
 # while you can run your local server like http://127.0.0.1:8000, this is created when i run the command.
 # if you change the command to http://http://127.0.0.1:8000/about it will show the second message.
 # if you change the command to http://http://127.0.0.1:8000/docs it will give you the documentation of your FastAPI
+
+
+@app.get("/view")
+def view():
+    print("--- Inside /view endpoint ---")
+    data = load_data()
+    return data
